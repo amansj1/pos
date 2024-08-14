560bb8f81c64c02b207bd5dbaec133fb434b39c0
Aman Sejahtera <51205338+amansj1@users.noreply.github.com>
2024-08-14 13:46:36 +0700

    update migration

--- migrations/20240812043654-logMigration.js
+++ migrations/20240812043654-logMigration.js
@@ -3,20 +3,39 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('log', { 
+      log_id: {type: Sequelize.INTEGER(15), allowNull: false, autoIncrement: true, primaryKey: true},
+      loc_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: false,
+        reference:{
+          model:{
+            tableName: 'location'
+          },
+          key:'loc_id'
+        }
+      },
+      func_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: true,
+        reference:{
+          model:{
+            tableName: 'func'
+          },
+          key:'func_id'
+        }
+      },
+      action:{type: Sequelize.TEXT('long'), allowNull:true},
+      data:{type: Sequelize.TEXT('long'), allowNull:true},
+      status:{type: Sequelize.STRING(8), allowNull:true},
+      user_id:{type: Sequelize.INTEGER(4),allowNull: false},
+      createdAt:{type: Sequelize.DATE, allowNull: false}
+    });
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     await queryInterface.dropTable('log');
+     
   }
 };
