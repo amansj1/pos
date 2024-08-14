14e43b379edb657c66dc4ca221c29976a481d826
Aman Sejahtera <51205338+amansj1@users.noreply.github.com>
2024-08-14 11:58:11 +0700

    migration update 2

--- migrations/20240812043529-accessMigration.js
+++ migrations/20240812043529-accessMigration.js
@@ -5,18 +5,52 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('access', { 
       acc_id: {
-        
-      }
+        type: Sequelize.INTEGER(8),
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      loc_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: false,
+        reference:{
+          model:{
+            tableName: 'location'
+          },
+          key:'loc_id'
+        }
+      },
+      func_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: false,
+        reference:{
+          model:{
+            tableName: 'func'
+          },
+          key:'func_id'
+        }
+      },
+      role_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: false,
+        reference:{
+          model:{
+            tableName: 'role'
+          },
+          key:'role_id'
+        }
+      },
+      create:{type: Sequelize.BOOLEAN, allowNull:false},
+      read:{type: Sequelize.BOOLEAN, allowNull:false},
+      update:{type: Sequelize.BOOLEAN, allowNull:false},
+      delete:{type: Sequelize.BOOLEAN, allowNull:false},
+
+      
     });
      
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('access');
   }
 };
--- migrations/20240812043541-userMigration.js
+++ migrations/20240812043541-userMigration.js
@@ -3,20 +3,54 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', { 
+      user_id: {type: Sequelize.INTEGER(4),allowNull: false, autoIncrement: true, primaryKey: true},
+      username: {type: Sequelize.STRING(10), allowNull:false},
+      role_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: false,
+        reference:{
+          model:{
+            tableName: 'role'
+          },
+          key:'role_id'
+        }
+      },
+      loc_id:{
+        type :Sequelize.INTEGER(2),
+        allowNull: false,
+        reference:{
+          model:{
+            tableName: 'location'
+          },
+          key:'loc_id'
+        }
+      },
+      name: {type: Sequelize.STRING(25), allowNull:false},
+      pass: {type: Sequelize.TEXT('long'), allowNull:false},
+      token: {type: Sequelize.TEXT('long'), allowNull:false},
+      email: {type: Sequelize.STRING(50), allowNull:false},
+      notes: {type: Sequelize.TEXT('long'), allowNull:false},
+      status: {type: Sequelize.STRING(5), allowNull:false},
+      createdAt:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      deletedAt:{
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+
+    });
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
+    
   }
 };
